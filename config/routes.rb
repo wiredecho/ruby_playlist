681b30a32281eea8fd7382cc850f12da51e4cdd6
wiredecho <wiredecho@gmail.com>
2016-07-28 11:43:45 -0700

    added logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "songs" => "sessions#songs"
   post "songs" => "songs#create"
   get 'songs/:id' =>"songs#index"
+  delete "sessions" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
